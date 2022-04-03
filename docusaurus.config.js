3d8f09ba102e720887ab265d081dfacdd08c82fa
IyotakeTatanka <schererse@gmx.de>
2022-04-03 23:11:24 +0200

    Updated edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,13 +27,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/the-tatanka/github-pages-docusaurus-template/tree/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/the-tatanka/github-pages-docusaurus-template/tree/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
